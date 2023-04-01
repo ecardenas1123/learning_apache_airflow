6c2395a1f66bd983121d1c118b177f60e574ba5e
Enzo Cardenas <ecardenasnicho@gmail.com>
2023-04-01 11:51:10 -0500

    mi first dag

---
+++ dags/0-firstdag.py
@@ -0,0 +1,13 @@
+from airflow import DAG
+from datetime import datetime
+from airflow.operators.empty import EmptyOperator
+
+with DAG(dag_id="firstdag",
+        description="Nuestro primer DAG",
+        start_date=datetime(2022, 7, 1),
+        schedule_interval="@once") as dag:
+
+    t1 = EmptyOperator(task_id="dummy")
+    t1
+
+    
